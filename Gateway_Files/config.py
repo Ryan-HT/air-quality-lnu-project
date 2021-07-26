d7f864ede649adb8b3e436eac46492fa097d27cb
Hamed Talebi <talebi.hd@gmail.com>
2021-07-26 07:08:48 +0200

    Move config to gateway folder

--- Gateway_Files/config.py
+++ Gateway_Files/config.py
@@ -5,10 +5,10 @@ WIFI_PASS = "<YOUR_WIFI_PASSWORD>"
 
 # MQTT serer's configuration
 MQTT_SERVER = "<YOUR_MQTT_SERVER_IP"
-MQTT_PORT = 1883
+MQTT_PORT = 0
 MQTT_USER = "<MQTT_USER_NAME"
 MQTT_KEY = "<MQTT_PASSWORD>"
-MQTT_CLIENT_ID = "id-1223"  # Can be anything start with id- 
+MQTT_CLIENT_ID = "id-1223"  # Can be anything start with id-
 MQTT_TEMPERATURE_FEED = "devices/temperature"
 MQTT_HUMIDITY_FEED = "devices/humidity"
 MQTT_POLLUTANT_FEED = "devices/pollutant"
@@ -17,7 +17,7 @@ MQTT_POLLUTANT_FEED = "devices/pollutant"
 SSL_FILES = dict([
     ("ca_certs", "/flash/cert/CA.pem"),
     ("keyfile", "/flash/cert/client-key.pem"),
-    ("certfile", "/flash/cert/clent-cert.pem")
+    ("certfile", "/flash/cert/client-cert.pem")
 ])
 
 # Other variables
